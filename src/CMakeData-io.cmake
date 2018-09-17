b8c2e885462a2779e4dbcf3a50f4d9ff9326240a
Joshua Shearer <Rychard@users.noreply.github.com>
2018-09-17 02:05:47 -0400

    p2io is similar to p3io, evidently

--- src/CMakeData-io.cmake
+++ src/CMakeData-io.cmake
@@ -1,10 +1,12 @@
 list(APPEND SMDATA_IO_SRC
+  "io/P2IO.cpp"
   "io/P3IO.cpp"
   "io/USBDevice_Libusb.cpp"
   "io/USBDriver.cpp"
 )
 
 list(APPEND SMDATA_IO_HPP
+  "io/P2IO.h"
   "io/P3IO.h"
   "io/USBDevice.h"
   "io/USBDriver.h"
